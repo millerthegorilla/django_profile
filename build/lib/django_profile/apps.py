7984b85fdd0cc8803c4871a0ca89927ec233f1cf
james miller <jamesstewartmiller@gmail.com>
2022-10-16 19:03:24 +0100

    added captcha dynamically

--- build/lib/django_profile/apps.py
+++ build/lib/django_profile/apps.py
@@ -3,6 +3,7 @@ from django import apps
 from django.conf import settings
 
 from django_users import templates as user_templates
+from captcha import templates as captcha_templates
 
 
 class DjangoProfileConfig(apps.AppConfig):
@@ -12,4 +13,7 @@ class DjangoProfileConfig(apps.AppConfig):
     def ready(self) -> None:
         settings.TEMPLATES[0]["DIRS"].append(
             os.path.abspath(user_templates.__path__._path[0])
+        )
+        settings.TEMPLATES[0]["DIRS"].append(
+            os.path.abspath(captcha_templates.__path__._path[0])
         )
